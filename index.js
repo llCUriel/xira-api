060ed91da5a263278c7002633b91196ecea7c629
Cesar Uriel <uuriel12009u@gmail.com>
2024-01-09 18:09:12 -0600

    new feature

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const cors  = require('cors');
+import express, { json } from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT ?? 1234;
 
-app.use(express.json());
+app.use(json());
 app.use(cors());
-app.use((req, res, next) => {
-    console.log('Time:', Date.now());
+
+app.use(async (req, res, next) => {
+    if (req.method === 'GET') 
+        await retrieveResponse(req);
     next();
 })
 
 
 app.get('/', (req, res) => {
-    res.send('Hello, worlß!');
+    req.result = req.result.filter(job => job.company == 'NVIDIA');
+    res.send(req.result);
 });
 
 
@@ -24,3 +28,9 @@ app.use((req,res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+async function retrieveResponse(req) {
+    req.result = await fetch('https://devitjobs.com/api/jobsLight')
+        .then(res => res.json());
+}
+
