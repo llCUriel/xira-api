b305106625306251595225990708d4e28b405477
Cesar Uriel <uuriel12009u@gmail.com>
2024-01-10 15:16:10 -0600

    hello world

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch';
+import express from 'express';
 
 const app = express();
-const port = process.env.PORT ?? 1234;
 
-app.use(json());
-app.use(cors());
-
-app.use(async (req, res, next) => {
-    if (req.method === 'GET') 
-        await retrieveResponse(req);
-    next();
-})
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    req.result = req.result.filter(job => job.company == 'NVIDIA');
-    res.send(req.result);
+    res.send('Hello World!');
+    
 });
 
-
-app.use((req,res) => {
-    res.status(404).send('404 Not Found');
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {    
+    console.log(`Listening on port ${port}`);   
 });
-
-async function retrieveResponse(req) {
-    req.result = await fetch('https://devitjobs.com/api/jobsLight')
-        .then(res => res.json());
-}
-
